01d77e67a9247abd18c754563414a49ce0bbd466
fegloff <fegloff@gmail.com>
2023-09-27 16:33:41 -0500

    add logger

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,12 @@ FROM python:3.8
 RUN pip install uvicorn
 RUN pip install chromadb==0.3.25
 RUN pip install --force-reinstall fastapi==0.85.1
-COPY server.py .
-CMD ["uvicorn", "server:app", "--host", "0.0.0.0", "--port", "8000"]
+
+COPY log_conf.yaml /app/
+COPY server.py /app/
+
+# Set the working directory to /app
+WORKDIR /app
+
+# COPY server.py .
+CMD ["uvicorn", "server:app", "--host", "0.0.0.0", "--port", "8000","--log-config","/app/log_conf.yaml"]
--- server.py
+++ server.py
@@ -1,6 +1,12 @@
+import logging
+import random
+import string
+import time
 import chromadb
 import chromadb.config
-from chromadb.server.fastapi import FastAPI
+from chromadb.server.fastapi import FastAPI, Request
+
+logger = logging.getLogger(__name__)
 
 settings = chromadb.config.Settings(
     chroma_db_impl = 'clickhouse',
@@ -10,3 +16,18 @@ settings = chromadb.config.Settings(
 
 server = FastAPI(settings)
 app = server.app()
+app.debug = True
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    idem = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    logger.info(f"rid={idem} start request path={request.url.path}")
+    start_time = time.time()
+    
+    response = await call_next(request)
+    
+    process_time = (time.time() - start_time) * 1000
+    formatted_process_time = '{0:.2f}'.format(process_time)
+    logger.info(f"rid={idem} completed_in={formatted_process_time}ms status_code={response.status_code}")
+    
+    return response
